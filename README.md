f2b0b56f49bdea8db31535ea2f7e8c7f0f09ecfe
samircd4 <samircd4@gmail.com>
2023-02-12 23:11:47 +0600

    initial

---
+++ README.md
@@ -0,0 +1,64 @@
+
+# Web scraping Project from Tawhid Vai
+
+This sript will scrape artvee.com website
+
+
+
+## Documentation
+
+Create a new folder on your computer and open with vs code editor.
+
+Now follow each step carefully.
+
+Open terminal and create virtual environment with following command.
+
+
+## Installation
+
+#### create a virtual environment 
+
+```bash
+  python -m venv env
+```
+#### Active virtual environment 
+
+```bash
+  env\Scripts\activate
+```
+#### install scrapy library
+
+```bash
+  pip install scrapy
+```
+#### create a scrapy project
+
+```bash
+  scrapy startproject artvee .
+```
+
+#### create a scrapy spider
+
+```bash
+  scrapy genspider art_vee x
+```
+
+## Modifications
+
+go to artvee/spiders directory and you'll find a file called art_vee.py.
+replace the art_vee.py file with my given art_vee.py file
+
+### method 1
+
+Delete all the code in art_vee.py file and paste all code from my given art_vee.py file
+
+### method 2
+Delete that art_vee.py file and add my given art_vee.py file at the same place
+
+## Now time to run that code on terminal
+
+```bash
+  scrapy crawl art_vee -o anyname.csv
+```
+### start scrapping 
+
---
+++ artvee/spiders/art_vee.py
@@ -0,0 +1,54 @@
+import scrapy
+
+
+class ArtVeeSpider(scrapy.Spider):
+    name = "art_vee"
+    classic = [
+    {'type': 'classical_art', 'category': 'Abstract', 'link': 'https://artvee.com/c/abstract/'},
+    {'type': 'classical_art', 'category': 'Figurative', 'link': 'https://artvee.com/c/figurative/'},
+    {'type': 'classical_art', 'category': 'Landscape', 'link': 'https://artvee.com/c/landscape/'},
+    {'type': 'classical_art', 'category': 'Religion', 'link': 'https://artvee.com/c/religion/'},
+    {'type': 'classical_art', 'category': 'Mythology', 'link': 'https://artvee.com/c/mythology/'},
+    {'type': 'classical_art', 'category': 'Posters', 'link': 'https://artvee.com/c/posters/'},
+    {'type': 'classical_art', 'category': 'Drawings', 'link': 'https://artvee.com/c/drawings/'},
+    {'type': 'classical_art', 'category': 'Illustration', 'link': 'https://artvee.com/c/illustration/'},
+    {'type': 'classical_art', 'category': 'Still Life', 'link': 'https://artvee.com/c/still-life/'},
+    {'type': 'classical_art', 'category': 'Animals', 'link': 'https://artvee.com/c/animals/'},
+    {'type': 'classical_art', 'category': 'Botanical', 'link': 'https://artvee.com/c/botanical/'},
+    {'type': 'classical_art', 'category': 'Asian Art', 'link': 'https://artvee.com/c/asian-art/'},
+    {'type': 'modern_art', 'category': 'Abstract', 'link': 'https://artvee.com/c/modern-art/abstract-modern-art/'},
+    {'type': 'modern_art', 'category': 'Figurative', 'link': 'https://artvee.com/c/modern-art/figurative-modern-art/'},
+    {'type': 'modern_art', 'category': 'Illustration', 'link': 'https://artvee.com/c/modern-art/illustration-modern-art/'},
+    {'type': 'modern_art', 'category': 'Surrealism', 'link': 'https://artvee.com/c/modern-art/surrealism/'},
+    {'type': 'modern_art', 'category': 'Posters', 'link': 'https://artvee.com/c/modern-art/posters-modern-art/'},
+    {'type': 'modern_art', 'category': 'Drawings', 'link': 'https://artvee.com/c/modern-art/drawings-modern-art/'},
+    {'type': 'modern_art', 'category': 'Pop Art', 'link': 'https://artvee.com/c/modern-art/pop-art/'},
+    {'type': 'modern_art', 'category': 'Still Life', 'link': 'https://artvee.com/c/modern-art/still-life-modern-art/'},
+    {'type': 'modern_art', 'category': 'Landscape', 'link': 'https://artvee.com/c/modern-art/landscape-modern-art/'},
+    {'type': 'modern_art', 'category': 'Animals', 'link': 'https://artvee.com/c/modern-art/animals-modern-art/'},
+    {'type': 'modern_art', 'category': 'Expressionism', 'link': 'https://artvee.com/c/modern-art/expressionism/'},
+    {'type': 'modern_art', 'category': 'Pop Surrealism', 'link': 'https://artvee.com/c/modern-art/pop-surrealism/'}
+    ]
+    def start_requests(self):
+        for url in self.classic[:1]:
+            d_type = url['type']
+            category = url['category']
+            link = url['link']
+            per_page_70 = '?per_page=70'
+            link_70 = f'{link}{per_page_70}'
+            yield scrapy.Request(url=link_70, callback=self.parse, meta={'d_type':d_type, 'category': category})
+    
+    def parse(self, response):
+        content = response.css('h3.product-title')
+        d_type = response.meta.get('d_type')
+        category = response.meta.get('category')
+        print(d_type , category)
+        for c in content:
+            title = c.css('a::text').get()
+            link = c.css('a::attr(href)').get()
+            yield {'type':d_type, 'category':category, 'title': title, 'link': link}
+        
+        next_page = response.css('ul.page-numbers li a.next::attr(href)').get()
+        if next_page is not None:
+            yield scrapy.Request(url= next_page, callback=self.parse, meta={'d_type':d_type, 'category': category})
+
